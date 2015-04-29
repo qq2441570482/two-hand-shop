3d28cc4680fa6b20f43cf7654b94b08ce946df8b
qq2441570482 <2441570482@qq.com>
2015-04-29 23:43:13 +0800

    do some small things

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,22 @@ Category.create(name: '体育用品')
 Category.create(name: '音乐乐器')
 Category.create(name: '寝室用品')
 Category.create(name: '服装服饰')
+Category.create(name: '其他')
 
 
 
 Major.create(name: '计算机科学与技术')
 Major.create(name: '软件工程')
+Major.create(name: '数学与应用数学')
+Major.create(name: '国际经济与贸易')
+Major.create(name: '公共事业管理')
+Major.create(name: '历史学')
+Major.create(name: '英语')
+Major.create(name: '哲学')
+Major.create(name: '微电子科学与工程')
+Major.create(name: '环境工程')
+Major.create(name: '生物工程')
+
 
 Grade.create(name: '2011级')
 Grade.create(name: '2012级')
